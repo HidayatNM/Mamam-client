f6f89040cc40aa861a9f997e59f1f61c6b0a6134
HidayatNM <hidayatnurmusa1@gmail.com>
2022-12-08 13:57:03 +0700

    deploy

--- customer/src/stores/counter.js
+++ customer/src/stores/counter.js
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-const baseUrl = "http://localhost:3000";
+const baseUrl = "https://mamam-production.up.railway.app";
 
 export const useCustomerStore = defineStore("customer", {
   state: () => {
